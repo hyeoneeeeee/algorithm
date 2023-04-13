42909ac62fbfac347a2e24882a45c1cae7c80fd8
명현 <z9x80123@gmail.com>
2023-04-13 22:46:58 +0900

    23/04/13

--- 브루트 포스/분해합.py
+++ 브루트 포스/분해합.py
@@ -1,1 +1,7 @@
 n=int(input())
+for i in range(1,n+1):
+    if i+sum(map(int,str(i))) ==n:
+        print(i)
+        break
+else:
+    print(0)
---
+++ 브루트 포스/체스판 다시 칠하기.py
@@ -0,0 +1,5 @@
+m,n=map(int, input().spllit())
+b=[]
+for _ in range(n):
+    b.append(input())
+for 
