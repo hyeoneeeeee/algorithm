c89e2988e456688ba6760b055612458e89492224
명현 <z9x80123@gmail.com>
2023-04-27 22:07:42 +0900

    23/04/27

--- 정렬/나이순 정렬.py
+++ 정렬/나이순 정렬.py
@@ -1,9 +1,9 @@
 import sys
 l=[]
-n=int(input())
+n=int(sys.stdin.readline())
 for i in range(n):
     y,name = map(str, sys.stdin.readline().split())
-    l.append([i,y,name])
-l.sort(key= lambda x:(x[1], x[0]))
+    l.append([int(y),name])
+l.sort(key= lambda x: x[0])
 for j in range(n):
-    print(f"{l[j][1]} {l[j][2]}")
+    print(f"{l[j][0]} {l[j][1]}")
