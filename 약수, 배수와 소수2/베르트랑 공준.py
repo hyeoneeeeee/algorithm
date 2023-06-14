b9d932508055b9e510f674831242813f1cc5c2a6
명현 <z9x80123@gmail.com>
2023-06-14 23:00:40 +0900

    23/06/14

--- 약수, 배수와 소수2/베르트랑 공준.py
+++ 약수, 배수와 소수2/베르트랑 공준.py
@@ -1,19 +1,22 @@
 import sys
+prime_list = []
 def get_prime(x):
-    for num in range(x+1, 2*x+1):
+    for num in range(2, 2*x+1):
         for i in range(2,int(num**0.5)+1):
             if num%i == 0:
                 break
         else:
             prime_list.append(num)
-
+get_prime(123456)
 while True:
+    c=0
     n=int(sys.stdin.readline().strip())
-    prime_list = []
     if n == 0:
         break
     elif n == 1:
         print(1)
     else:
-        get_prime(n)
-        print(len(prime_list))
+        for i in prime_list:
+            if n < i <= 2*n:
+                c += 1
+        print(c)
